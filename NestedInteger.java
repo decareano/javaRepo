d7fa961d803ac5973e9c09c1b2efda84f5f77dcc
Marcelo Gobelli <almagro@comcast.net>
2022-08-23 10:24:29 -0700

    NestedInteger.java
    work in progress to try different files

---
+++ NestedInteger.java
@@ -0,0 +1,14 @@
+
+ 
+ public interface NestedInteger {
+ 
+      
+      public boolean isInteger();
+ 
+      
+     public Integer getInteger();
+ 
+     
+      public List<NestedInteger> getList();
+ }
+ 
